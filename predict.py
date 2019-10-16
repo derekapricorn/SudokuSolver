9c6deb82246e26b949e376f305c563e90a618df1
Derek Zhi <derekz@curacloudcorp.com>
2019-10-16 15:58:04 -0700

    sync

--- predict.py
+++ predict.py
@@ -1,6 +1,10 @@
+"""
+Given a model and an image, predict what digit the image contains
+"""
 import imutils
 import sys
 import numpy as np
+import argparse
 import torch
 import torchvision
 import matplotlib.pyplot as plt
@@ -10,6 +14,7 @@ from torchvision import datasets, transforms
 from torch import nn, optim
 import cv2
 from torchvision.transforms import ToTensor, Normalize
+from CNN import CNN
 
 
 def toImage(net_output):
@@ -26,33 +31,26 @@ def validate_input(img_in):
     plt.imshow(imutils.opencv2matplotlib(resized))
     plt.show()
 
-img_in = sys.argv[2]
-img = cv2.imread(img_in)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-resized = cv2.resize(gray, (28, 28))
-img = ToTensor()(resized)
-img = Normalize((0.5,) , (0.5,))(img)
-net_input = Variable(img).view(1, 784)
 
 
-input_size = 784
-hidden_sizes = [128, 64]
-output_size = 10
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("image_file")
+    parser.add_argument("--model")
+    args = parser.parse_args()
+    print("load input image")
+    image = cv2.imread(args.image_file)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    resized = cv2.resize(gray, (28, 28))
+    net_input = Normalize((0.5,) , (0.5,))(ToTensor()(resize))
 
-model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
-                      nn.ReLU(),
-                      nn.Linear(*hidden_sizes),
-                      nn.ReLU(),
-                      nn.Linear(hidden_sizes[1], output_size),
-                      nn.LogSoftmax(dim=1)
-                      )
-
-model.load_state_dict(torch.load(sys.argv[1]))
-model.eval()
-with torch.no_grad():
-    logps = model(net_input )
-ps = torch.exp(logps)
-pred = np.argmax(ps.numpy()[0])
-print("predicting ", pred)
+    model = CNN()
+    model.load_state_dict(torch.load(args.model))
+    model.eval()
+    with torch.no_grad():
+        logps = model(net_input)
+        ps = torch.exp(logps)
+        pred = np.argmax(ps.numpy()[0])
+        print("predicting ", pred)
 
 
